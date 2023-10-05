f1051ae7b4a484d4f7f9a09e523878786f1e4165
Vladislav Tarasov <tarvlad@inbox.ru>
2023-10-05 15:46:21 +0700

    1

--- ApproxSPH.cpp
+++ ApproxSPH.cpp
@@ -8,10 +8,10 @@ int main() {
     Request request;
     request.leftBorder = -1.0l;
     request.rightBorder = 2.0l;
-    request.nGasParticlesPerUnitSide = 1000;
+    request.nGasParticlesPerUnitSide = 100;
     request.h = 0.1l;
     request.tau = pow(10.0l, -8);
-    request.timeMoment = 1000.0l * request.tau;
+    request.timeMoment = 100.0l * request.tau;
     request.initRho = 1;
     request.u0 = 0.1l;
     request.k = 2.0l * acos(-1.0l);
@@ -20,6 +20,7 @@ int main() {
 
     Kernel kernel(&request);
     kernel.compute<WENDLAND, 1, 2, 2, 1, 1, 1, 0>();
+    freopen("output.txt", "w", stdout);
     std::cout << kernel.getComputeRes() << std::endl;
 
     return 0;
